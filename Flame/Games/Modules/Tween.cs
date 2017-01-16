f96a8fd0907c88d8b91f9b7099fe83085d4dbb93
roonilwazlib7 <roonilwazlib7@hotmail.com>
2017-01-16 00:07:52 -0700

    Generics and tweens

--- Flame/Games/Modules/Tween.cs
+++ Flame/Games/Modules/Tween.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Flame.Games.Modules
 {
+    public enum Easing
+    {
+        Smooth,
+        Linear
+    }
     public class Tween: Module
     {
         public Tween(Game game): base(game)
@@ -15,6 +21,32 @@ namespace Flame.Games.Modules
         public void Update()
         {
 
+        }
+        public TweenSetup<T> CreateTween<T>(T objectToTween, double time, Easing easing = Easing.Smooth)
+        {
+            return new TweenSetup<T>(objectToTween);
+        }
+        public void CreateSpline()
+        {
+
+        }
+    }
+
+    public class TweenObject
+    {
+
+    }
+
+    public class TweenSetup<T>
+    {
+        T _objectToTween;
+        public TweenSetup(T objectToTween)
+        {
+            _objectToTween = objectToTween;
+        }
+        public void From(T Start, T End)
+        {
+            
         }
     }
 }
